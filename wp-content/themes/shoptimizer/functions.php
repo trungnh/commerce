4a845256f2849834aefd734e6838954bdb5a7469
Trung, Nguyen Huu <trungnh28@gmail.com>
2021-08-25 10:55:03 +0700

    update

--- wp-content/themes/shoptimizer/functions.php
+++ wp-content/themes/shoptimizer/functions.php
@@ -526,3 +526,34 @@ function woo_add_custom_general_fields_save( $post_id )
 		update_post_meta( $post_id, '_landing_checkout', esc_attr( $woocommerce_text_field ) );
 
 }
+
+/**
+Remove all possible fields
+ **/
+function wc_remove_checkout_fields( $fields ) {
+
+	// Billing fields
+	unset( $fields['billing']['billing_company'] );
+	unset( $fields['billing']['billing_email'] );
+	unset( $fields['billing']['billing_state'] );
+	unset( $fields['billing']['billing_last_name'] );
+	unset( $fields['billing']['billing_address_2'] );
+	unset( $fields['billing']['billing_city'] );
+	unset( $fields['billing']['billing_postcode'] );
+	unset( $fields['billing']['billing_country'] );
+
+	// Shipping fields
+	unset( $fields['shipping']['shipping_company'] );
+	unset( $fields['shipping']['shipping_state'] );
+	unset( $fields['shipping']['shipping_last_name'] );
+	unset( $fields['shipping']['shipping_address_2'] );
+	unset( $fields['shipping']['shipping_city'] );
+	unset( $fields['shipping']['shipping_postcode'] );
+	unset( $fields['shipping']['shipping_country'] );
+
+	// Order fields
+	//unset( $fields['order']['order_comments'] );
+
+	return $fields;
+}
+add_filter( 'woocommerce_checkout_fields', 'wc_remove_checkout_fields' );
